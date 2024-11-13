26e16b0aacc0a7dce78a5e87da2d48c3a6e231c4
nianrui <810199886@qq.com>
2024-11-13 16:44:21 +0800

    增加了2位数乘法和显示答案的逻辑

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,31 @@ import 'dart:io';
 import 'dart:math';
 import 'dart:ui' as ui;
 
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = WindowOptions(size: Size(1650, 850), center: true, title: "应该给年宏博出多少道题呢");
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(MyApp());
-  doWhenWindowReady(() {
+
+/*  doWhenWindowReady(() {
     final window = appWindow;
     const initialSize = ui.Size(1650, 680);
     window.minSize = initialSize;
     window.size = initialSize;
     window.alignment = Alignment.center;
-    window.title = "应该给年宏博出多少道题呢";
+    window.title = "";
     window.show();
-  });
+  });*/
 }
 
 class MyApp extends StatelessWidget {
@@ -60,19 +68,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   List<Bean> arr = [];
+
   bool debugNeedsPaint = true;
+
+  // region  ---- 输入题目数量 ----
   late TextEditingController editingController;
+
+  // endregion
+  // region  ---- 默认显示今天的日期，点击日期可以选择日期 ----
   late DateTime selectedTime;
-  late bool visible;
+
+  // endregion
+  // region  ---- 主页是否显示，答案是否显示 ----
+  late bool dashboardVisibility, answerVisibility = false;
+
+  // endregion
+  // region  ---- 题目数量，图层索引 ----
   late int quantity, index = 0;
 
-  //输入的值，接收键盘输入和擦除尾端
+  // endregion
+
   String content = "";
 
   @override
   void initState() {
     super.initState();
-    visible = true;
+    dashboardVisibility = true;
     selectedTime = DateTime.now();
     editingController = TextEditingController(text: "");
     // createBean();
@@ -95,9 +116,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
               children: [
                 Expanded(
                     child: TextField(
+                  onEditingComplete: () => inputDone(),
                   maxLength: 2,
                   inputFormatters: [FilteringTextInputFormatter(RegExp(r"\d"), allow: true)],
                   maxLines: 1,
+                  textInputAction: TextInputAction.done,
                   controller: editingController,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
@@ -107,18 +130,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                           borderSide: BorderSide(color: Colors.blueGrey, width: 2)),
                       hintText: "请输入数字"),
                 )),
-                IconButton(
-                    onPressed: () {
-                      if (editingController.value.text.isNotEmpty) {
-                        setState(() {
-                          String result = editingController.value.text;
-                          quantity = int.parse(result);
-                          createBean();
-                          index = 1;
-                        });
-                      }
-                    },
-                    icon: Icon(Icons.double_arrow_sharp))
+                IconButton(onPressed: () => inputDone(), icon: Icon(Icons.double_arrow_sharp))
               ],
             ),
           ),
@@ -142,19 +154,31 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         ),
                         Spacer(),
                         Visibility(
-                          child: TextButton(
-                              onPressed: () {
+                          visible: dashboardVisibility,
+                          child: commonButton(
+                              title: "返回主页",
+                              fun: () {
                                 setState(() {
                                   index = 0;
-                                  visible = true;
+                                  dashboardVisibility = true;
                                 });
-                              },
-                              child: Text("返回主页")),
-                          visible: visible,
+                              }),
                         ),
+                        SizedBox(width: 10),
                         Visibility(
-                          child: TextButton(onPressed: () => onButtonClicked(), child: Text("截屏并保存")),
-                          visible: visible,
+                          visible: dashboardVisibility,
+                          child: commonButton(
+                              title: answerVisibility == false ? "显示答案" : "隐藏答案",
+                              fun: () {
+                                setState(() {
+                                  answerVisibility = !answerVisibility;
+                                });
+                              }),
+                        ),
+                        SizedBox(width: 10),
+                        Visibility(
+                          visible: dashboardVisibility,
+                          child: commonButton(title: "截屏并保存", fun: () => onButtonClicked()),
                         )
                       ],
                     ),
@@ -171,7 +195,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                                   width: 220,
                                   color: index % 2 == 0 ? Colors.black12.withAlpha(20) : Colors.white,
                                   child: Text(
-                                    arr[index].label ?? "",
+                                    "${arr[index].label}${answerVisibility == true ? arr[index].answer : ""}",
                                     style: TextStyle(fontSize: 22),
                                   ),
                                 ))
@@ -186,7 +210,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     ));
   }
 
-  //随机生成0-9的数字按键，然后增加擦除和随机按键
+  // region  ---- 创造一个个题目和答案 ----
   void createBean() {
     arr = List.generate(quantity, (index) {
       int num = Random().nextInt(OperatorType.values.length);
@@ -194,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       return bean;
     });
     int left, right, count = 1;
-    arr.forEach((bean) {
+    for (var bean in arr) {
       left = Random().nextInt(999);
       right = Random().nextInt(999);
       while (!diagnose(operator: bean.type!, left: left, right: right)) {
@@ -202,11 +226,28 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         right = Random().nextInt(999);
       }
       bean.label = "($count)  $left${bean.type?.label}$right=";
+      bean.answer = getAnswer(operator: bean.type!, left: left, right: right);
       count++;
-    });
-    debugPrint("");
+    }
   }
 
+  // endregion
+
+  // region  ---- 按键盘回车键或者点击了向右箭头显示题目 ----
+  void inputDone() {
+    if (editingController.value.text.isNotEmpty) {
+      setState(() {
+        String result = editingController.value.text;
+        quantity = int.parse(result);
+        createBean();
+        index = 1;
+      });
+    }
+  }
+
+  // endregion
+
+  // region  ---- 出题合法性挣断 ----
   bool diagnose({required OperatorType operator, required int left, required int right}) {
     bool result = true;
     switch (operator) {
@@ -232,14 +273,43 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           result = false;
         } else {}
         break;
+      case OperatorType.multiplication:
+        if (left < 2 || right < 2) {
+          result = false;
+        } else if (!((left < 100 && right < 10) || (left < 10 && right < 100))) {
+          result = false;
+        } else {}
+        break;
     }
     return result;
   }
 
-  //按钮被点击
+  // endregion
+
+  // region  ---- 返回答案 ----
+  int getAnswer({required OperatorType operator, required int left, required int right}) {
+    late int answer;
+    switch (operator) {
+      case OperatorType.plus:
+        answer = left + right;
+        break;
+      case OperatorType.minus:
+        answer = left - right;
+        break;
+      case OperatorType.multiplication:
+        answer = left * right;
+        break;
+      }
+    return answer;
+  }
+
+  // endregion
+
+
+  // region  ---- 截屏并且在指定路径输出图片 ----
   void onButtonClicked() {
     setState(() {
-      visible = false;
+      dashboardVisibility = answerVisibility;
     });
     Future.delayed(Duration(milliseconds: 200), () {
       getImageData().then((value) {
@@ -248,11 +318,12 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       });
     });
   }
+  // endregion
 
   Uint8List? uint8list;
   GlobalKey repaint = GlobalKey();
 
-  /// 获取截取图片的数据
+  // region  ---- 获取截取图片的数据 ----
   Future<Uint8List> getImageData() async {
     BuildContext buildContext = repaint.currentContext!;
     RenderObject? boundary = buildContext.findRenderObject();
@@ -265,7 +336,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       return getImageData();
     }
     // 获取当前设备的像素比
-    double dpr = ui.window.devicePixelRatio;
+    double dpr = View.of(context).devicePixelRatio;
+    // double dpr = ui.window.devicePixelRatio;
     // pixelRatio 代表截屏之后的模糊程度，因为不同设备的像素比不同
     // 定义一个固定数值显然不是最佳方案，所以以当前设备的像素为目标值
     ui.Image image = await (boundary as RenderRepaintBoundary).toImage(pixelRatio: dpr);
@@ -275,13 +347,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     // 返回图片的数据
     return imageBytes!;
   }
+  // endregion
 
   downloadPicture() async {
     // final directory = await getApplicationDocumentsDirectory();
     // final filePath = path.join(directory.path, 'image.png');
     // String path = "D:/code/salary_sheet/assets/capture/";
     String path = "C:/Users/Administrator/Desktop/";
-    final file = File("$path年宏博的数学题.png");
+    final file = File("$path${answerVisibility?"数学题答案.png":"年宏博的数学题.png"}");
     await file.writeAsBytes(uint8list!);
     /*for(int i=0 ; i<arrayListSigned.length-1;i++){
       int j = i+1;
@@ -303,43 +376,59 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     ).then((value) => {
           // debugPrint("showDatePicker")
           setState(() {
-            selectedTime = value!;
+            selectedTime = value??DateTime.now();
           })
         });
   }
 }
 
+MaterialButton commonButton({required String title, required Function fun}) {
+  return MaterialButton(
+    onPressed: () {
+      fun.call();
+    },
+    color: Colors.blue,
+    textColor: Colors.white,
+    child: Text(title),
+  );
+}
+
+// region  ---- 数据结构 ----
 class Bean {
+  int? answer;
   String? label;
   OperatorType? type;
 
-  Bean.fromParams({this.label, this.type});
+  Bean.fromParams({this.answer, this.label, this.type});
 
   factory Bean(Object jsonStr) => jsonStr is String ? Bean.fromJson(json.decode(jsonStr)) : Bean.fromJson(jsonStr);
 
   static Bean? parse(jsonStr) => ['null', '', null].contains(jsonStr) ? null : Bean(jsonStr);
 
   Bean.fromJson(jsonRes) {
+    answer = jsonRes['answer'];
     label = jsonRes['label'];
     type = jsonRes['type'];
   }
 
   @override
   String toString() {
-    return '{"label": ${label != null ? '${json.encode(label)}' : 'null'}, "type": ${type != null ? '${json.encode(type)}' : 'null'}}';
+    return '{"answer": $answer, "label": ${label != null ? json.encode(label) : 'null'}, "type": ${type != null ? json.encode(type) : 'null'}}';
   }
 
-  String toJson() => this.toString();
+  String toJson() => toString();
 }
+// endregion
 
-//按钮类型：数字、擦除、随机
-enum ButtonType { number, wipe, random }
-
+// region  ---- 枚举:加减乘除 ----
 enum OperatorType {
   plus(label: "+"),
-  minus(label: "-");
+  minus(label: "-"),
+  multiplication(label: "×");
+  // division(label: "÷");
 
   final String label;
 
   const OperatorType({required this.label});
 }
+// endregion
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
+import screen_retriever_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
